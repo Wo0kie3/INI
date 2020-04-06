efec31bed49f398b37a018dfd20edd9a5ad3efb8
Wo0kie3 <44375336+Wo0kie3@users.noreply.github.com>
2020-04-06 14:53:31 +0200

    Update program.c

--- program.c
+++ program.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <locale.h>
 
 int main(int argc, char* argv[])
 {
+	int action = -1;
 	FILE* fp = fopen(argv[1], "r");
 	if(!fp) {
         perror("File opening failed");
@@ -17,16 +20,22 @@ int main(int argc, char* argv[])
 			char *part1 = strtok(target, " ");
 			char *znak = strtok(NULL," ");
 			char *part2 = strtok(NULL, " ");
-			char *sec1 = strtok(part1,".");
+			const char *sec1 = strtok(part1,".");
 			char *key1 = strtok(NULL, ".");
-			char *sec2 = strtok(part2,".");
+			const char *sec2 = strtok(part2,".");
 			char *key2 = strtok(NULL, ".");
-
-			printf("%s ", sec1);
-			printf("%s ", key1);
-			printf("%s ", znak);
-			printf("%s ", sec2);
-			printf("%s\n", key2);
+			char *beginB = "[";
+			char *endB = "]";
+			char par1[strlen(sec1)+1];
+			char par2[strlen(sec2)+1];
+			strcpy(par1,beginB);
+			strcpy(par2,beginB);
+			strcat(par1,sec1);
+			strcat(par2,sec2);
+			strcat(par1,endB);
+			strcat(par2,endB);
+			printf("%s %s \n", par1,par2);
+			action = 1;
 		}
 		else
 		{
@@ -35,17 +44,33 @@ int main(int argc, char* argv[])
 
 			char *section = strtok(target, ".");
 			char *key = strtok(NULL, ".");
-
-			printf("%s\n",section);
-			printf("%s\n",key);
+			char *beginB = "[";
+			char *endB = "]";
+			char par1[strlen(section)+1];
+			strcpy(par1,beginB);
+			strcat(par1,section);
+			strcat(par1,endB);
+			printf("%s\n", par1 );
+			action = 2;
 		}
-
-    char c[30]; // note: int, not char, required to handle EOF
-    while (fgets(c,sizeof(c),fp) != NULL)
+    char c[30];
+    while (fgets(c,sizeof(c),fp)!= NULL)
 		{
-    	printf("%s", c);
+			//tworzenie struktur
     }
+		//wyświetlanie poleceń
+		if(action == 1)
+		{
 
+		}
+		else if(action == 2)
+		{
+
+		}
+		else
+		{
+			printf("%s\n", "Command not recognized!");
+		}
     if (ferror(fp))
         puts("I/O error when reading");
     else if (feof(fp))
